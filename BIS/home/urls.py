248ad5fca3da917979eb70a63a72ac145f57128e
AhmedSalam22 <Ahmed0salam22@gmail.com>
2020-08-14 13:06:29 +0200

    Base

---
+++ BIS/home/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path , include
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path("" , TemplateView.as_view(template_name = "home/index.html") , name="home") , 
+    path('accounts/', include('django.contrib.auth.urls')),
+]
