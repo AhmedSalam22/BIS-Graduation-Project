ac214be9294945cc91e8c42d879c9f5ad5fa19a4
AhmedSalam22 <Ahmed0salam22@gmail.com>
2020-08-16 00:08:48 +0200

    Sole. accounts modify

--- BIS/sole_proprietorship/urls.py
+++ BIS/sole_proprietorship/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path, reverse_lazy
+from django.views.generic import TemplateView
 from . import views
 
-app_name='sole_propritorship'
+app_name='sole_proprietorship'
 urlpatterns = [
-    path('', views.AccountsListView.as_view(), name='all'),
-    path('accounts/create', 
-        views.AccountsCreateView.as_view(success_url=reverse_lazy('sole_propritorship:all')), name='accounts_create'),
-    path('accounts/<int:pk>/update', 
-        views.AccountsUpdateView.as_view(success_url=reverse_lazy('sole_propritorship:all')), name='accounts_update'),
-    path('accounts/<int:pk>/delete', 
-        views.AccountsDeleteView.as_view(success_url=reverse_lazy('sole_propritorship:all')), name='accounts_delete'),
+    path("" , TemplateView.as_view(template_name= "sole_proprietorship/index.html")),
+    path('/accounts', views.AccountsListView.as_view(), name='all'),
+    path('/accounts/create', 
+        views.AccountsCreateView.as_view(success_url=reverse_lazy('sole_proprietorship:all')), name='accounts_create'),
+    path('/accounts/<int:pk>/update', 
+        views.AccountsUpdateView.as_view(success_url=reverse_lazy('sole_proprietorship:all')), name='accounts_update'),
+    path('/accounts/<int:pk>/delete', 
+        views.AccountsDeleteView.as_view(success_url=reverse_lazy('sole_proprietorship:all')), name='accounts_delete'),
 ]
 
 # We use reverse_lazy in urls.py to delay looking up the view until all the paths are defined
