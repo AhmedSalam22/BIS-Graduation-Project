9af5e1904b9544dc5d7757710032bc629f6337f6
AhmedSalam22 <Ahmed0salam22@gmail.com>
2020-09-07 05:21:56 +0200

    herkou

--- BIS/BIS/settings.py
+++ BIS/BIS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6ang@@6j)r5^x3ldie5#a2mxo*5^8cy$)2wton(ws7opwz)(cg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
 
 
 # Application definition
