8bfcc82d386800bdeeb8456c1cf6817d5e427971
AhmedSalam22 <Ahmed0salam22@gmail.com>
2020-12-14 20:49:13 +0200

    admin customization 2

--- BIS/result/admin.py
+++ BIS/result/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Result
+from django.utils.safestring import mark_safe
+
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ['id' ,  'description', 'name', 'uploaded_file' , 'get_share']
+    list_display_links = ['id' , 'description']
+    list_editable = ['uploaded_file' ]
+
+    def get_share(self , obj):
+        return mark_safe(f'<a href="http://127.0.0.1:8000/result/query/{obj.share}" target="_blank"> \
+        http://127.0.0.1:8000/result/query/{obj.share}</a>')
+
+    get_share.short_description = "Shareable Link"
+
 # Register your models here.
-admin.site.register(Result)
+admin.site.register(Result , ResultAdmin)
--- BIS/sole_proprietorship/admin.py
+++ BIS/sole_proprietorship/admin.py
@@ -1,9 +1,36 @@
 from django.contrib import admin
 from .models import Journal , Accounts
+
+
+class AccountsAdmin(admin.ModelAdmin):
+    list_display = ( 'account' , 'normal_balance' , 'account_type' , 'get_owner' )
+    list_editable = ('normal_balance' , 'account_type')
+    list_filter = ['normal_balance' , 'account_type' , 'owner']
+
+    def get_owner(self , rec):
+        return rec.owner.username
+
+  
+
+    get_owner.short_description = "Owner"
+    actions_on_bottom = True
+
+
+class JournalAdmin(admin.ModelAdmin):
+    list_display = ('account' , 'date' , 'balance' , 'transaction_type' , 'comment' , 'owner')
+    list_editable = ['comment']
+    search_fields = ['comment']
+    list_filter = ['account', 'transaction_type' , 'owner']
+    sortable_by = ['account' ,'date' ,'balance' , 'date' , 'transaction_type' ,  'owner']
+    ordering = ['-date' , 'owner']
+
+    actions_on_bottom = True
+
 # Register your models here.
-admin.site.register([Journal , Accounts])
+admin.site.register(Accounts , AccountsAdmin)
+admin.site.register(Journal , JournalAdmin)
 
-
+# Customize Django Admin
 admin.site.site_header = "AYBA -Automate Your Business Activity- by Ahmed Maher"
 admin.site.site_title = "AYBA"
 admin.site.index_title = "Dashboard"
