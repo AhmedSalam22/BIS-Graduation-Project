774dabb4cdc6d3ff15f20595c0860a535f3909d9
AhmedSalam22 <Ahmed0salam22@gmail.com>
2020-12-15 20:45:30 +0200

    admin customize 3

--- BIS/Customers_Sales/admin.py
+++ BIS/Customers_Sales/admin.py
@@ -1,7 +1,54 @@
 from django.contrib import admin
 from .models import Customer , CustomerEmail , Telephone , CustomerType , CustomerAddress , CustomerNote
+
+
+class CustomerAddressInline(admin.TabularInline):
+    model = CustomerAddress
+    show_change_link = True
+
+    def get_extra(self , request , obj=None , **kwargs):
+        if obj:
+            return 1
+        return 3
+
+    
+
+class CustomerEmailInline(admin.TabularInline):
+    model = CustomerEmail
+    
+    def get_extra(self , request , obj=None , **kwargs):
+        if obj:
+            return 1
+        return 3
+
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name' , 'middle_name' , 'last_name' , 'account_number' , 'customer_type']
+    list_filter = ['customer_type' , 'prospect' , 'inactive']
+    search_fields =['first_name' , 'middle_name' , 'last_name' , 'account_number']
+    inlines = [CustomerAddressInline , CustomerEmailInline]
+
+    save_on_top = True
+
+    fieldsets = (
+        ("Main", {
+            "fields": [
+                'owner' ,('first_name' , 'middle_name' , 'last_name' ), 'customer_type'
+            ],
+        }),
+        ('Optionl', {
+            "fields" : ['prospect' , 'inactive'] ,
+            "classes" : ('collapse',)
+        }),
+    )
+    
+
+
 # Register your models here.
-admin.site.register([Customer ,
+
+admin.site.register(Customer , CustomerAdmin)
+admin.site.register([
  CustomerEmail , 
  Telephone , 
  CustomerType , 
--- BIS/sole_proprietorship/admin.py
+++ BIS/sole_proprietorship/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from .models import Journal , Accounts
 
 
+
+class JournalInline(admin.TabularInline):
+    model = Journal
+
 class AccountsAdmin(admin.ModelAdmin):
     list_display = ( 'account' , 'normal_balance' , 'account_type' , 'get_owner' )
     list_editable = ('normal_balance' , 'account_type')
     list_filter = ['normal_balance' , 'account_type' , 'owner']
+    fields = ( 'owner' , ('account' , 'normal_balance' , 'account_type' ))
+    inlines = [JournalInline]
 
     def get_owner(self , rec):
         return rec.owner.username
@@ -23,6 +29,19 @@ class JournalAdmin(admin.ModelAdmin):
     list_filter = ['account', 'transaction_type' , 'owner']
     sortable_by = ['account' ,'date' ,'balance' , 'date' , 'transaction_type' ,  'owner']
     ordering = ['-date' , 'owner']
+    # fields = ('owner' , ('account' , 'date' , 'balance' , 'transaction_type' ) , 'comment')
+
+    fieldsets = (
+        ("Main", {
+            "fields": ['owner' , ('account' , 'date' , 'balance' , 'transaction_type' )],
+        }),
+        ("optional" , {
+            "fields": ["comment"],
+            "classes": ('collapse',)
+        }
+        )
+    )
+    
 
     actions_on_bottom = True
 
