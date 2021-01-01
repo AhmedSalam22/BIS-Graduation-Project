009c52b8ee5a1c65ade49776cfd0b8e564185621
AhmedSalam22 <Ahmed0salam22@gmail.com>
2021-01-01 14:14:20 +0200

    enable search on projects

--- BIS/projects/urls.py
+++ BIS/projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from .views import ProjectsListView , ProjectsDetailView
+from .views import ProjectsListView , ProjectsDetailView , ProjectsListViewAjax
 app_name = "projects"
 urlpatterns = [
     path("" , ProjectsListView.as_view(), name="home"), 
-    path("/<int:pk>" , ProjectsDetailView.as_view() , name="detail")
+    path("/<int:pk>" , ProjectsDetailView.as_view() , name="detail"),
+    path("/ProjectsListViewAjax/<str:search>" , ProjectsListViewAjax.as_view() , name="listAjax")
 ]
--- BIS/projects/views.py
+++ BIS/projects/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView , DetailView
 from .models import Projects
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from django.db.models import Q
+from django.views import View
+from django.http import  HttpResponse
+from django.core.serializers import serialize
+
 # Create your views here.
 
 class ProjectsListView(ListView):
@@ -10,3 +16,19 @@ class ProjectsListView(ListView):
 class ProjectsDetailView(DetailView):
     model = Projects
     template_name = 'projects/detail.html'
+
+
+class ProjectsListViewAjax(View):
+    def get(self, request , search) :
+        if search !="None" :
+            # Multi-field search
+            query = Q(title__contains=search)
+            query.add(Q(detail__contains=search), Q.OR)
+            query.add(Q(tag__contains=search), Q.OR)
+            objects = Projects.objects.filter(query).select_related()
+        else :
+            objects = Projects.objects.all()
+
+        data = serialize('json', objects)
+        return HttpResponse(data, content_type="application/json")
+
